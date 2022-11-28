bb138d5b37057aab27b1ccbb902d2cfbdd2f77db
ccoderJava <congccoder@gmail.com>
2022-11-28 22:39:37 +0800

    :sparkles: add util.jwt and JWT middleware

---
+++ pkg/util/jwt.go
@@ -0,0 +1,49 @@
+package util
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"go-gin/pkg/setting"
+	"time"
+)
+
+var jwtSecret = []byte(setting.JwtSecret)
+
+type Claims struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	jwt.StandardClaims
+}
+
+//GenerateToken 生成签名token
+//1. 生成签名字符串，再使用字符串生成完成签名token
+func GenerateToken(username, password string) (string, error) {
+	nowTime := time.Now()
+	expireTime := nowTime.Add(3 * time.Hour)
+	claims := Claims{
+		username,
+		password,
+		jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer:    "gin-blog",
+		},
+	}
+
+	//使用的加签算法方式
+	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := tokenClaims.SignedString(jwtSecret)
+	return token, err
+}
+
+//ParseToken 用于验证签名
+// Valid内部 验证给予时间的声明：exp,iat,nbf。
+func ParseToken(token string) (*Claims, error) {
+	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+	if tokenClaims != nil {
+		if claims, ok := tokenClaims.Claims.(*Claims); ok && tokenClaims.Valid {
+			return claims, nil
+		}
+	}
+	return nil, err
+}
