a5553d244b479623d1bb3d0402690115e96472d2
ccoderJava <congccoder@gmail.com>
2022-12-01 15:06:19 +0800

    :sparkles: main中采用endless热更新：创建子进程后将原进程退出

--- main.go
+++ main.go
@@ -2,26 +2,39 @@ package main
 
 import (
 	"fmt"
-	"github.com/fvbock/endless"
 	"go-gin/pkg/setting"
 	"go-gin/routers"
-	"log"
-	"syscall"
+	"net/http"
 )
 
 func main() {
-	endless.DefaultReadTimeOut = setting.ReadTimeout
-	endless.DefaultWriteTimeOut = setting.WriteTimeout
-	endless.DefaultMaxHeaderBytes = 1 << 20
-	endPoint := fmt.Sprintf(":%d", setting.HttpPort)
+	router := routers.InitRouter()
 
-	//返回一个初始化的endlessServer, 在BeforeBegin时输出当前进程pid.
-	server := endless.NewServer(endPoint, routers.InitRouter())
-	server.BeforeBegin = func(add string) {
-		log.Printf("Actual pid is %d", syscall.Getpid())
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HttpPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
 	}
-	err := server.ListenAndServe()
+	err := s.ListenAndServe()
 	if err != nil {
-		log.Printf("Server err:%v", err)
+		return
 	}
+
+	//优雅重启热更新
+	//endless.DefaultReadTimeOut = setting.ReadTimeout
+	//endless.DefaultWriteTimeOut = setting.WriteTimeout
+	//endless.DefaultMaxHeaderBytes = 1 << 20
+	//endPoint := fmt.Sprintf(":%d", setting.HttpPort)
+	//
+	////返回一个初始化的endlessServer, 在BeforeBegin时输出当前进程pid.
+	//server := endless.NewServer(endPoint, routers.InitRouter())
+	//server.BeforeBegin = func(add string) {
+	//	log.Printf("Actual pid is %d", syscall.Getpid())
+	//}
+	//err := server.ListenAndServe()
+	//if err != nil {
+	//	log.Printf("Server err:%v", err)
+	//}
 }
